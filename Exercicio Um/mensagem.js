397c7b9c6df7128ccb03d44ce9aac5e98a85e755
mariasteles <telesmariaeduarda12@gmail.com>
2026-02-11 16:07:19 -0300

    Exercício Um

---
+++ Exercicio Um/mensagem.js
@@ -0,0 +1,7 @@
+let nome = "Maria";
+let curso = "Técnico em Desenvolvimento de Sistema";
+let mensagemMotivadora = "O sucesso é a soma de pequenos esforços repetidos dia após dia";
+
+console.log("Nome: ", nome);
+console.log("Curso: ", curso);
+console.log("Mensagem Motivadora: ", mensagemMotivadora);
