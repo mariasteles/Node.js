ec31004710266f6742ad8bcc6c087eb9f267d6bc
mariasteles <telesmariaeduarda12@gmail.com>
2026-02-11 16:07:30 -0300

    Exercício Dois

---
+++ Exercicio Dois/calculadora.js
@@ -0,0 +1,12 @@
+let numero1 = 15;
+let numero2 = 25;
+
+let soma = numero1 + numero2;
+let subtracao = numero1 - numero2;
+let multiplicacao = numero1 * numero2;
+let divisao = numero1 / numero2;
+
+console.log("Soma: ", soma);
+console.log("Subtracao: ", subtracao);
+console.log("Multiplicacao: ", multiplicacao);
+console.log("Divisao: ", divisao);
