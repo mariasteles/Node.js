6afdeb282d2e956035cb6ef700bf8f6bbb845b2d
mariasteles <telesmariaeduarda12@gmail.com>
2026-02-11 17:13:33 -0300

    Exercício Tres

---
+++ Exercicio Tres/variaveis.js
@@ -0,0 +1,11 @@
+let nome = "Mariluda";
+let classe = "Ilusionista";
+let nível = "50";
+let vida = "7000";
+let mana = "6990";
+
+console.log("Nome: ", nome);
+console.log("Classe: ", classe);
+console.log("Nível: ", nível);
+console.log("Vida: ", vida);
+console.log("Mana: ", mana);
