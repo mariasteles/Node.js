f000d155fe062f2dd9ce3e988140c95be0ebf483
mariasteles <telesmariaeduarda12@gmail.com>
2026-02-12 14:59:38 -0300

    Exercício Um

---
+++ Parte Dois/Exercicio Um/operacoes.js
@@ -0,0 +1,25 @@
+function somar(a, b) {
+    return a + b;
+  }
+  
+  function subtrair(a, b) {
+    return a - b;
+  }
+  
+  function multiplicar(a, b) {
+    return a * b;
+  }
+  
+  function dividir(a, b) {
+    if (b === 0) {
+      return "Erro: divisão por zero não é permitida.";
+    }
+    return a / b;
+  }
+  
+  module.exports = {
+    somar,
+    subtrair,
+    multiplicar,
+    dividir
+  };
