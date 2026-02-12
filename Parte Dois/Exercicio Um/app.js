7bd47445c16856f091937d46af2f3ab6502161a4
mariasteles <telesmariaeduarda12@gmail.com>
2026-02-12 15:23:18 -0300

    Exercícios Um

---
+++ Parte Dois/Exercicio Um/app.js
@@ -0,0 +1,11 @@
+const operacoes = require('./operacoes');
+
+const num1 = 10;
+const num2 = 5;
+
+console.log("Soma:", operacoes.somar(num1, num2));
+console.log("Subtração:", operacoes.subtrair(num1, num2));
+console.log("Multiplicação:", operacoes.multiplicar(num1, num2));
+console.log("Divisão:", operacoes.dividir(num1, num2));
+
+console.log("Divisão por zero:", operacoes.dividir(num1, 0));
